8e9f9b4dea9c472c5c4e9038fc737a068ca6afa7
Daniel Heredia <danielheredia13@gmail.com>
2023-06-22 11:14:33 -0600

    server fixed

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ app.use("/api/contact", contactRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/front-end/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
